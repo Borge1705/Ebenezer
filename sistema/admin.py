3fd2bfd7bc8948668c0899cff53ea83249fecd5f
Valeria <vborge05@gmail.com>
2026-06-15 00:03:40 -0600

    admingroup

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,19 +1,5 @@
-# admin.py
-
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.models import Group
-from django.db.utils import OperationalError, ProgrammingError
-
-
-# Crear grupos solo cuando la BD ya esté disponible
-try:
-    Group.objects.get_or_create(name='Administrador')
-    Group.objects.get_or_create(name='Tecnico')
-
-except (OperationalError, ProgrammingError):
-    pass
-
 
 admin.site.register(Telefono)
 admin.site.register(Cliente)
